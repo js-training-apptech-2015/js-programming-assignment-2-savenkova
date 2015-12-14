a233e9e784d2a443070c46b928a1a2c4d0ac02a3
gorka <gorka@maximail.org>
2015-12-14 14:33:20 +0500

    + solution 2, 3

--- script.js
+++ script.js
@@ -14,11 +14,28 @@ function solution1( A ) {
     }
     return min;
 };
-/*
-function solution2(S) {
 
+function solution2(S) {
+     var stack = [];
+     for (var i = 0; i < S.length; i++) {
+        if (S[i] == '{' || S[i] == '[' || S[i] == '(') {
+            stack.push(S[i]);
+        } else {
+            if (stack.length == 0) {
+                return false;
+            }
+            var symbol = stack.pop();
+            if (S[i] == ')' && symbol != '(' || S[i] == ']' && symbol != '[' || S[i] == '}' && symbol != '{') {
+                return false;
+            }
+        }
+     }
+     return stack.length == 0;
 };
 
 function solution3(A, F) {
-
-};*/
+    var re = /(\((.*)\)|(\w+))\s*=>\s*(.*)/;
+    var match = re.exec(F);
+    var func = new Function(match[2] || match[3], "return " + match[4]);
+    return A.map(func);
+};
